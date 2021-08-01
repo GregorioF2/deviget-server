072f5194f8217e73047c668ae1f59f099a739741
gregorio <gfreidin@dc.uba.ar>
2021-07-31 21:02:35 -0300

    First version of cache done. Missing controller to test

--- priceService/transparentCache.go
+++ priceService/transparentCache.go
@@ -14,29 +14,26 @@ type cacheEntry struct {
 type TransparentCache struct {
 	actualPriceService PriceService
 	maxAge             time.Duration
-	prices             map[string]cacheEntry
-
-	pricesMutex sync.Mutex
+	prices             sync.Map
 }
 
 func NewTransparentCache(actualPriceService PriceService, maxAge time.Duration) *TransparentCache {
 	return &TransparentCache{
 		actualPriceService: actualPriceService,
 		maxAge:             maxAge,
-		prices:             map[string]cacheEntry{},
+		prices:             sync.Map{},
 	}
 }
 
 func (cache *TransparentCache) GetPriceFor(itemCode string) (float64, error) {
-	cache.pricesMutex.Lock()
-	entry, ok := cache.prices[itemCode]
+	var entry *cacheEntry
+	v, ok := cache.prices.Load(itemCode)
 	if ok {
+		entry = v.(*cacheEntry)
 		timePassed := time.Since(entry.creationTime)
 		if timePassed > cache.maxAge {
-			delete(cache.prices, itemCode)
-			cache.pricesMutex.Unlock()
+			cache.prices.Delete(itemCode)
 		} else {
-			cache.pricesMutex.Unlock()
 			return entry.value, nil
 		}
 	}
@@ -45,24 +42,40 @@ func (cache *TransparentCache) GetPriceFor(itemCode string) (float64, error) {
 	if err != nil {
 		return 0, fmt.Errorf("getting price from service : %v", err.Error())
 	}
-	cache.pricesMutex.Lock()
-	cache.prices[itemCode] = cacheEntry{value: price, creationTime: time.Now()}
-	cache.pricesMutex.Unlock()
-
+	cache.prices.Store(itemCode, &cacheEntry{value: price, creationTime: time.Now()})
 	return price, nil
 }
 
-// GetPricesFor gets the prices for several items at once, some might be found in the cache, others might not
-// If any of the operations returns an error, it should return an error as well
-func (c *TransparentCache) GetPricesFor(itemCodes ...string) ([]float64, error) {
+func (cache *TransparentCache) GetPricesFor(itemCodes ...string) ([]float64, error) {
 	results := []float64{}
-	for _, itemCode := range itemCodes {
-		// TODO: parallelize this, it can be optimized to not make the calls to the external service sequentially
-		price, err := c.GetPriceFor(itemCode)
+
+	var wg sync.WaitGroup
+	wg.Add(len(itemCodes))
+
+	out := make(chan float64, 1)
+	errs := make(chan error, 1)
+
+	getPriceAsync := func(itemCode string) {
+		defer wg.Done()
+		price, err := cache.GetPriceFor(itemCode)
 		if err != nil {
-			return []float64{}, err
+			errs <- err
+		} else {
+			out <- price
 		}
-		results = append(results, price)
+	}
+
+	for _, itemCode := range itemCodes {
+		go getPriceAsync(itemCode)
+	}
+	wg.Wait()
+	close(out)
+	close(errs)
+	if len(errs) > 0 {
+		return nil, <-errs
+	}
+	for val := range out {
+		results = append(results, val)
 	}
 	return results, nil
 }
